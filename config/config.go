03d0540abbc70403b3c144844af2f81086c3a621
d3liciae <irynka2207@gmail.com>
2020-05-31 15:06:15 +0300

    start changes

--- config/config.go
+++ config/config.go
@@ -4,9 +4,4 @@ const (
 	ServerPort      = ":3000"
 	BotToken        = ""
 	RegisterWebhook = true
-<<<<<<< HEAD
 	ServerUrl       = "" //Update URL after starting ngrok
-=======
-	ServerUrl       = "https://bfa581c6a7cb.ngrok.io" //Update URL after starting ngrok
->>>>>>> d9feba197c80a39099c324fb2c55c88ddd7c8f3e
-)
