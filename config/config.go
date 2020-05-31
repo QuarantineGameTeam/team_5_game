d14beca90871d17214cedebcb50274eae347c340
d3liciae <irynka2207@gmail.com>
2020-05-31 14:35:46 +0300

    Added database connection

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	ServerPort      = ":3000"
-	BotToken        = "1029941261:AAHcNgFCSfBIBHdF3vWengqLS4YthZJRtXw"
+	BotToken        = ""
 	RegisterWebhook = true
-	ServerUrl       = "https://15c990c4f7b6.ngrok.io" //Update URL after starting ngrok
+	ServerUrl       = "" //Update URL after starting ngrok
 )
