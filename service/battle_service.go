679503e5e44d1edf771399af2397c758672108ca
bukv <bumusic13@gmail.com>
2020-06-03 13:08:03 +0300

    add battleground

--- service/battle_service.go
+++ service/battle_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"log"
 	"team_5_game/model/telegram"
 )
@@ -19,5 +20,54 @@ func SendStartBattleMessage(callbackQuery *telegram.CallbackQuery) {
 
 func ProcessBattleStarting(callbackQuery *telegram.CallbackQuery) {
 	EditMessageReplyMarkup(callbackQuery.Message.Chat.ID, callbackQuery.Message.MessageID, nil)
-	//TODO:  Implement saving choice for the user and start battle
+	youAre := fmt.Sprintf("You are: %v", "🔵")
+	SendMessage(callbackQuery.Message.Chat.ID, youAre, nil)
+	SendBattlefield(callbackQuery)
+}
+
+func SendBattlefield(callbackQuery *telegram.CallbackQuery) {
+	EditMessageReplyMarkup(callbackQuery.Message.Chat.ID, callbackQuery.Message.MessageID, nil)
+	replyMarkup := telegram.NewInlineKeyboardMarkup(
+		telegram.NewInlineKeyboardRow(
+			telegram.NewInlineKeyboardButtonData("🔹", "PRESS_1"),
+			telegram.NewInlineKeyboardButtonData("⚪️", "PRESS_2"),
+			telegram.NewInlineKeyboardButtonData("🔹", "PRESS_3"),
+			telegram.NewInlineKeyboardButtonData("⚪️", "PRESS_4"),
+			telegram.NewInlineKeyboardButtonData("⚪️", "PRESS_5"),
+		),
+
+		telegram.NewInlineKeyboardRow(
+			telegram.NewInlineKeyboardButtonData(" 🔹 ", "PRESS_6"),
+			telegram.NewInlineKeyboardButtonData(" 🔹 ", "PRESS_7"),
+			telegram.NewInlineKeyboardButtonData(" 🔹 ", "PRESS_8"),
+			telegram.NewInlineKeyboardButtonData("🔺🔸", "PRESS_9"),
+			telegram.NewInlineKeyboardButtonData(" ⚪️ ", "PRESS_10"),
+		),
+
+		telegram.NewInlineKeyboardRow(
+			telegram.NewInlineKeyboardButtonData(" ⚪️ ", "PRESS_11"),
+			telegram.NewInlineKeyboardButtonData(" 🔹 ", "PRESS_12"),
+			telegram.NewInlineKeyboardButtonData(" 🔺 ", "PRESS_13"),
+			telegram.NewInlineKeyboardButtonData(" 🔸 ", "PRESS_14"),
+			telegram.NewInlineKeyboardButtonData(" 🔸 ", "PRESS_15"),
+		),
+
+		telegram.NewInlineKeyboardRow(
+			telegram.NewInlineKeyboardButtonData(" ⚪️ ", "PRESS_16"),
+			telegram.NewInlineKeyboardButtonData("🔵🔺", "PRESS_17"),
+			telegram.NewInlineKeyboardButtonData(" ⚪️ ", "PRESS_18"),
+			telegram.NewInlineKeyboardButtonData(" 🔺 ", "PRESS_19"),
+			telegram.NewInlineKeyboardButtonData(" 🔸 ", "PRESS_20"),
+		),
+
+		telegram.NewInlineKeyboardRow(
+			telegram.NewInlineKeyboardButtonData(" ⚪️ ", "PRESS_21"),
+			telegram.NewInlineKeyboardButtonData(" ⚪️ ", "PRESS_22"),
+			telegram.NewInlineKeyboardButtonData(" ⚪️ ", "PRESS_23"),
+			telegram.NewInlineKeyboardButtonData(" 🔺 ", "PRESS_24"),
+			telegram.NewInlineKeyboardButtonData(" 🔸 ", "PRESS_25"),
+		),
+	)
+
+	SendMessage(callbackQuery.Message.Chat.ID, "Select the cell you want to capture:", &replyMarkup)
 }
