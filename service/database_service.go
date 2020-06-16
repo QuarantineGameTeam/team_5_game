db1468d6344609eaa48f5c09f88afc5d850171c4
Serge Chernov <gres.cher@icloud.com>
2020-06-16 13:41:43 +0300

    battlefield works with new database.Battle struct + remove obsolete functions
    battlefield_service.go -> delete obsolete types and functions
    battle_service.go -> add IsFull() from deleted battlefield_service.go
    
    new functions to add a new battle type record to DB + reset option for a record that already exists in DB
    
    battle_service.go: SendBattlefield() -> optimize arguments
    
    battle_service.go -> rename resetBattle() to renameExistingBattle()
    
    some changes
    
    isthere isfull
    
    resolve conflicts with develop + remove obsolete functions

--- service/database_service.go
+++ service/database_service.go
@@ -71,42 +71,66 @@ func SaveUserToDB(user *database.User) error {
 func AppendUserTrack(callbackQuery *telegram.CallbackQuery, position int) {
 	log.Println("Start track saving")
 
-	user, err := GetUserFromDB(callbackQuery.From.ID)
+	//user, err := GetUserFromDB(callbackQuery.From.ID)
+	battle, err := GetBattleFromDB(callbackQuery.From.ID)
 	if err != nil {
 		log.Println("Could not get user", err)
 	}
-	user.Track[position-1] = position
+	battle.Sector[position-1].OwnedBy[0] = callbackQuery.From.ID
+	battle.Sector[position-1].IsCaptured = true
 
-	out, err := json.Marshal(user)
+	out, err := json.Marshal(battle)
 	if err != nil {
-		log.Println("Could not marshal user", err)
+		log.Println("Could not marshal battle", err)
 	}
 
-	err = redisClient.Set(context, userPrefix+strconv.FormatInt(callbackQuery.From.ID, 10), string(out), 0).Err()
+	err = redisClient.Set(context, battlePrefix+strconv.FormatInt(callbackQuery.From.ID, 10), string(out), 0).Err()
 	if err != nil {
 		log.Println("Could not save track", err)
 	}
 }
 
-func ClearUserTrack(callbackQuery *telegram.CallbackQuery) {
-	log.Println("Start track clearing")
+func GetBattleFromDB(id int64) (*database.Battle, error) {
+	log.Println("Get battle from DB, battle ID", id)
 
-	user, err := GetUserFromDB(callbackQuery.From.ID)
-	if err != nil {
-		log.Println("Could not get user", err)
-	}
-	var tmp [25]int
-	user.Track = tmp
+	result, err := redisClient.Get(context, battlePrefix+strconv.FormatInt(id, 10)).Result()
+	if err == redis.Nil {
+		log.Println("Battle not found", err)
+		return nil, err
+	} else if err != nil {
+		log.Println("Could not read battle from DB", err)
+		return nil, err
+	} else {
+		battle := &database.Battle{}
 
-	out, err := json.Marshal(user)
+		err := json.Unmarshal([]byte(result), battle)
+		if err != nil {
+			log.Println("Could not unmarshal battle", err)
+			return nil, err
+		}
+
+		log.Println("Battle successfully received from DB, ID", battle.ID)
+		return battle, nil
+	}
+}
+
+func SaveBattleToDB(battle *database.Battle) error {
+	log.Println("Save battle to the DB, ID", battle.ID)
+
+	out, err := json.Marshal(battle)
 	if err != nil {
-		log.Println("Could not marshal user", err)
+		log.Println("Could not marshal battle", err)
+		return err
 	}
 
-	err = redisClient.Set(context, userPrefix+strconv.FormatInt(callbackQuery.From.ID, 10), string(out), 0).Err()
+	err = redisClient.Set(context, battlePrefix+strconv.FormatInt(battle.ID, 10), string(out), 0).Err()
 	if err != nil {
-		log.Println("Could not save cleared track", err)
+		log.Println("Could not save battle", err)
+		return err
 	}
+
+	log.Println("Battle successfully saved to DB, ID", battle.ID)
+	return nil
 }
 
 func GetBattleFromDB(id int64) (*database.Battle, error) {
