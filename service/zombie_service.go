f694fe03bf20d0fe90f49e5697a5b41b329b6590
bukv <bumusic13@gmail.com>
2020-06-28 13:26:40 +0300

    new walking logic for zombiePlayer

--- service/zombie_service.go
+++ service/zombie_service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"log"
+	"math/rand"
 	"team_5_game/model/database"
 	"team_5_game/model/telegram"
+	"time"
 )
 
 func LaunchZombieUser(callbackQuery *telegram.CallbackQuery) {
@@ -22,7 +24,7 @@ func LaunchZombieUser(callbackQuery *telegram.CallbackQuery) {
 	}
 
 	zombie.ID = user.ID + 2
-	zombie.CurrentPos = Clans[zombie.ClanID].StartPosition - 1
+	zombie.CurrentPos = Clans[zombie.ClanID].StartPosition
 	zombie.CurrentBattle = user.CurrentBattle
 
 	SaveUserToDB(&zombie)
@@ -30,17 +32,42 @@ func LaunchZombieUser(callbackQuery *telegram.CallbackQuery) {
 }
 
 func ZombieWalking(callbackQuery *telegram.CallbackQuery) {
+	var cellChoice int
+	var isCaptured bool = true
 	zombie, err := GetUserFromDB(callbackQuery.From.ID + 2)
 	if err != nil {
 		log.Println("Could not get ZombieUser", err)
 		return
 	}
-
-	if zombie.CurrentPos == 24 {
-		zombie.CurrentPos = 0
-	} else {
-		zombie.CurrentPos++
+	battle, err := GetBattleFromDB(zombie.CurrentBattle)
+	if err != nil {
+		log.Println("Could not get Battle for ZombiUser", err)
+		return
 	}
+
+	walkRadius := ZombieWalkRadius(zombie.CurrentPos)
+
+	for _, cell := range walkRadius {
+		if cell > 0 && cell < 26 {
+			isCaptured = battle.Sector[cell-1].IsCaptured
+			if !isCaptured { // Free cell search
+				cellChoice = cell
+				break
+			}
+		}
+	}
+
+	// If there are no free cells (isCaptured == true)
+	for isCaptured {
+		rand.Seed(time.Now().UnixNano())
+		cellChoice = walkRadius[rand.Intn(len(walkRadius)-1)] // Choise random cell
+		if cellChoice > 0 && cellChoice < 26 {
+			break
+		}
+	}
+
+	zombie.CurrentPos = cellChoice
+	log.Println("ZombiePlayer POSITION:", zombie.CurrentPos)
 	SaveUserToDB(zombie)
 	ZombiePositionCapture(zombie)
 }
@@ -52,8 +79,36 @@ func ZombiePositionCapture(zombie *database.User) {
 		return
 	}
 
-	battle.Sector[zombie.CurrentPos].OwnedBy[1] = zombie.ID
-	battle.Sector[zombie.CurrentPos].IsCaptured = true
+	battle.Sector[zombie.CurrentPos-1].OwnedBy[1] = zombie.ID
+	battle.Sector[zombie.CurrentPos-1].IsCaptured = true
 
 	SaveBattleToDB(battle)
+}
+
+func ZombieWalkRadius(position int) []int {
+	var availableTerritory []int
+	switch {
+	case position%5 == 0:
+		availableTerritory = append(availableTerritory, position-1)
+		availableTerritory = append(availableTerritory, position+4)
+		availableTerritory = append(availableTerritory, position-6)
+		break
+	case position%5 == 1:
+		availableTerritory = append(availableTerritory, position+1)
+		availableTerritory = append(availableTerritory, position+6)
+		availableTerritory = append(availableTerritory, position-4)
+		break
+	default:
+		availableTerritory = append(availableTerritory, position-6)
+		availableTerritory = append(availableTerritory, position-4)
+		availableTerritory = append(availableTerritory, position+1)
+		availableTerritory = append(availableTerritory, position-1)
+		availableTerritory = append(availableTerritory, position+4)
+		availableTerritory = append(availableTerritory, position+6)
+	}
+
+	availableTerritory = append(availableTerritory, position+5)
+	availableTerritory = append(availableTerritory, position-5)
+
+	return availableTerritory
 }
